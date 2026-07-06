d1bbec1e25f8f4a0508c1bf64848c122bcecc7d3
murugesh <murugeshsekar96@gmail.com>
2026-07-06 15:43:55 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello from CI/CD Pipeline 🚀</h1>
+    <h2>GitHub + Jenkins + Docker + Ansible</h2>
+    <p>Deployment Successfully Done With Automation!</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
