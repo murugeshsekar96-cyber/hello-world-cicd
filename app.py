19a4494eddff11ba639982057d53cb9ad9d4c1a2
murugesh <murugeshsekar96@gmail.com>
2026-07-06 15:55:37 +0000

    edited app file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     return """
     <h1>Hello from CI/CD Pipeline 🚀</h1>
     <h2>GitHub + Jenkins + Docker + Ansible</h2>
-    <p>Deployment Successfully Done With Automation!</p>
+    <p>Deployment Successfully Done With Automation Only!</p>
     """
 
 if __name__ == "__main__":
